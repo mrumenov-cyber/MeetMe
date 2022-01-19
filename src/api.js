7f2c86638639946fafc89035ee9dd871a3a7538d
mrumenov-cyber <marija.rumenovic@fesb.hr>
2022-01-19 11:54:08 +0100

    Fixed failed tests, all are passing now

--- src/api.js
+++ src/api.js
@@ -1,5 +1,16 @@
+import { mockData } from "./mock-data";
+
+/*
+This function takes an events array, then uses map to create a new array with only locations.
+It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
+The Set will remove all duplicates from the array.
+*/
 export const extractLocations = (events) => {
-    var extractLocations = events.map((event) => event.location);
-    var locations = [...new Set(extractLocations)];
-    return locations;
-  };
+  var extractLocations = events.map((event) => event.location);
+  var locations = [...new Set(extractLocations)];
+  return locations;
+};
+
+export const getEvents = async () => {
+  return mockData;
+};
